cb4afa7db84f482cda6a2caebbc7174e4a68c9f0
michal221 <146548792+michallanger22@users.noreply.github.com>
2024-03-03 13:50:58 +0100

    Update

---
+++ w?.bat
@@ -0,0 +1,38 @@
+@echo off
+echo Witaj!
+pause
+echo o
+pause
+echo  o
+pause
+echo   o
+pause
+echo     o
+pause
+echo       o
+pause
+echo         o
+pause
+echo           o
+pause
+echo             o
+pause
+echo               o
+pause
+echo              o
+pause
+echo             o
+pause
+echo           o
+pause
+echo         o
+pause
+echo       o
+pause
+echo     o
+pause
+echo   o
+pause
+echo  o
+pause
+
